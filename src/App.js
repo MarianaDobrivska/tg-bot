9c4dbf02cd9209b4e0057f07e2fe2474706ecc11
Victor Malinovskyi <dobrivska.marry@gmail.com>
2023-12-20 10:39:43 +0200

    add id

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ let tg = window.Telegram.WebApp;
 function App() {
   const user = `${tg.initDataUnsafe.user.first_name}
 ${tg.initDataUnsafe.user.last_name}
-${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
+${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code}) ${tg.initDataUnsafe.user.id}`;
   return <div className="App">{user}</div>;
 }
 
